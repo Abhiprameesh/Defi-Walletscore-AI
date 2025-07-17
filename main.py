0ad1a405e9f86bbe433b9b30b41d6c985e7aa2b0
Abhinand Prameesh <abhiprameesh@gmail.com>
2025-07-17 17:13:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ with open("user_transactions.json", "r") as f:
 
 print("✅ JSON loaded. Total transactions:", len(transactions))
 
-# Step 2: Group transactions by wallet
+# Group transactions by wallet
 wallet_dict = {}
 
 for tx in transactions:
@@ -19,7 +19,7 @@ for tx in transactions:
 
 print("✅ Grouped transactions by wallet. Total unique wallets:", len(wallet_dict))
 
-# Step 3: Compute scores
+# Compute scores
 wallet_scores = []
 
 for wallet_address, tx_list in wallet_dict.items():
@@ -30,7 +30,7 @@ for wallet_address, tx_list in wallet_dict.items():
         "score": score
     })
 
-# Step 4: Save to CSV
+# Save to CSV
 df_scores = pd.DataFrame(wallet_scores)
 df_scores.to_csv("wallet_scores.csv", index=False)
 print("✅ All scores saved to wallet_scores.csv")
